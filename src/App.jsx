f4723df32af0a7090090db269c4e9e32882cb745
Ashwary <ashwary.jharbade999@gmail.com>
2021-11-23 18:55:49 +0530

    Worked on react theming

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Main Component</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,33 @@
+const Sidebar = () => {
+  const tabs = [
+    "What is React ?",
+    "Why to learn React ?",
+    "How to implement React ?",
+    "What is JSX ?",
+    "What are React components ?",
+    "React state",
+    "React props",
+    "React class component",
+    "React functional component",
+  ];
+
+  const _sidebarItemIteratorHandler = () => {
+    return tabs.map((item, index) => {
+      return (
+        <>
+          <div key={index} className="sidebarItems">
+            {item}
+          </div>
+        </>
+      );
+    });
+  };
+
+  return (
+    <>
+      <div className="sidebar">{_sidebarItemIteratorHandler()}</div>
+    </>
+  );
+};
+
+export default Sidebar;
